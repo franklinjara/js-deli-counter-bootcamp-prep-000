bbc2062b7573fe051989d2abbca747125668363a
Franklin <makyfj@hotmail.com>
2019-04-10 19:34:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function currentLine(line) {
     return "The line is currently empty."
   }
   
-  var number = `The line is currently: 1. ${line[0]}`;
+  var number = `The line is currently: 1. ${line[]}`;
   
   for (var i = 0; i < line.length; i++){
     number += `, ${1+i}. ${line[i]}`;
