e9fa78822e0a92cfec48d5b9547a3c9774583c44
Franklin <makyfj@hotmail.com>
2019-04-10 18:55:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(currentLine, nowServing) {
   for (var i = 0; i <= nowServing.length; i++) {
-    var welcome = console.log(`Welcome, ${nowServing}. You are number ${i + 1} in line`);
+    katzDe
   }
   return welcome;
 }
