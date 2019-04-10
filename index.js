751b0004ec0bc9e3e6803b6e963cc8ccb9bba4cc
Franklin <makyfj@hotmail.com>
2019-04-10 19:32:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function currentLine(line) {
   var number = `The line is currently: 1. ${line[0]}`;
   
   for (var i = 0; i < line.length; i++){
-    number += 
+    number += ``;
   }
  
   return "The line is currently: " + number; 
